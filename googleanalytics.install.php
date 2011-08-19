09c5dc9b98ef0900146d38639e33467d69c10d61
Will Steinmetz <willsteinmetz@me.com>
2011-08-19 18:05:40 -0400

    Set up info and install files

--- googleanalytics.install.php
+++ googleanalytics.install.php
@@ -1,23 +1,12 @@
 <?php
-
-class %MODULE%_install {
+/**
+ * @file googleanalytics.install.php
+ * @author Will Steinmetz
+ */
+class googleanalytics_install {
 	public function install() {
 		$mod = new Modules();
-		$mod->getModuleByName('%MODULE%');
-		$perms = array();
-		
-		$perm_ids = FabriqModules::register_perms($mod->id, $perms);
-		
-		global $db;
-		$sql = "";
-		$db->query($sql);
-		
-		// map paths
-		$pathmap = &FabriqModules::module('pathmap');
-		$pathmap->register_path('%PATH%', '%MODULE%', '%ACTION%', 'module');
-
-		// register events
-		FabriqModules::register_handler('%EVENTMODULE%', '%EVENTACTION%', '%EVENT%', '%MODULE%', '%ACTION%');
+		$mod->getModuleByName('googleanalytics');
 		
 		// set module as installed
 		$mod->installed = 1;
@@ -30,14 +19,14 @@ class %MODULE%_install {
 			global $configs;
 		}
 		$configs = new ModuleConfigs();
-		$configs->getForModule('%MODULE%');
+		$configs->getForModule('googleanalytics');
 		if ($_FAPP['templating']) {
 			FabriqTemplates::set_var('module_configs', $configs);
 		}
 		
 		if (isset($_POST['submit'])) {
-			$configs[$configs->configs['%CONFIG%']]->val = $_POST['%CONFIG'];
-			$configs->update($configs->configs['%CONFIG%']);
+			$configs[$configs->configs['apikey']]->val = $_POST['%CONFIG'];
+			$configs->update($configs->configs['apikey']);
 			Fabriq::render('none');
 			echo json_encode(array('success' => true));
 		}
@@ -45,19 +34,7 @@ class %MODULE%_install {
 	
 	public function uninstall() {
 		$mod = new Modules();
-		$mod->getModuleByName('%MODULE%');
-		
-		// remove perms
-		FabriqModules::remove_perms($mod->id);
-		
-		// remove paths
-		$pathmap = &FabriqModules::module('pathmap');
-		$pathmap->remove_path('%PATH%');
-		
-		// delete database table
-		global $db;
-		$sql = "DROP TABLE `fabmod_%MODULE%_%TABLE%`;";
-		$db->query($sql);
+		$mod->getModuleByName('googleanalytics');
 		
 		// set module as not installed
 		$mod->installed = 0;
--- googleanalytics.module.php
+++ googleanalytics.module.php
@@ -1,10 +1,13 @@
 <?php
 /**
- * @file
+ * @file googleanalytics.module.php
  * @author Will Steinmetz
+ * 
+ * This module provides the functionality for adding the code for Google Analytics to your website.
+ * An API key will need to be entered in the module config.
  */
 
-class %MODULE%_module extends FabriqModule {
+class googleanalytics_module extends FabriqModule {
 	function __construct() {
 		parent::__construct();
 	}
